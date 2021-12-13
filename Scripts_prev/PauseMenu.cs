bf07c4617d22f6d99f1fb67c6b6022896d84bb51
Seerow0 <92154813+Seerow0@users.noreply.github.com>
2021-12-13 10:21:17 -0500

    MyScripts

---
+++ Scripts_prev/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool gameispaused = false;
+
+	public GameObject pauseMenuUI;
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+			
+		{
+	if (gameispaused)
+				
+	{
+		Resume();
+
+	} else 
+	{
+		Pause();
+}
+			//Screen.lockCursor = false;
+			//Screen.lockCursor = true;
+}
+
+}
+public void Resume()
+{
+	pauseMenuUI.SetActive(false);
+	Time.timeScale = 1f;
+	gameispaused = false;
+	Screen.lockCursor = true;
+}
+void Pause ()
+
+{
+	pauseMenuUI.SetActive(true);
+	Time.timeScale = 0f;
+	gameispaused = true; 
+	Screen.lockCursor = false;
+	
+}
+	public void LoadMenu()
+	{
+		Time.timeScale = 1f; 
+		SceneManager.LoadScene ("Menu");
+	}
+	public void QuitGame()
+	{
+		Debug.Log("Quitting game...");
+		Application.Quit ();
+	}
+}
